a6b5924ec6b65affc70cc3128c5f8838a570f0a1
Raphaël Laporte <rl@pcsol.be>
2016-02-14 11:56:12 +0100

    from eclipse engine & utils

---
+++ app/src/main/java/rl/ht/be/calc40/core/Engine.java
@@ -0,0 +1,82 @@
+package rl.ht.be.calc40.core;
+
+import rl.ht.be.calc40.utils.Utils;
+
+public class Engine {
+
+	private static Double a = null;
+	private static String aStr = null;
+	private static Double b = null;
+	private static String bStr = null;
+	private static String op = null;
+
+	public static Double mult() {
+		if (a == null || b == null) {
+			throw new RuntimeException("ERREUR CALCUL");
+		}
+		return a * b;
+	}
+
+	public static Double div() {
+		if (a == null || b == null) {
+			throw new RuntimeException("ERREUR CALCUL");
+		}
+		return a / b;
+	}
+
+	public static Double minus() {
+		if (a == null || b == null) {
+			throw new RuntimeException("ERREUR CALCUL");
+		}
+		return a - b;
+	}
+
+	public static Double plus() {
+		if (a == null || b == null) {
+			throw new RuntimeException("ERREUR CALCUL");
+		}
+		return a + b;
+	}
+
+	public static Double pow() {
+		if (a == null || b == null) {
+			throw new RuntimeException("ERREUR CALCUL");
+		}
+		return Math.pow(a, b);
+	}
+
+	public static Double getA() {
+		return a;
+	}
+
+	public static void setA(String a) {
+		Engine.aStr = a;
+		Engine.a = Utils.parseString(a);
+	}
+
+	public static Double getB() {
+		return b;
+	}
+
+	public static void setB(String b) {
+		Engine.bStr = b;
+		Engine.b = Utils.parseString(b);
+	}
+
+	public static String getaStr() {
+		return aStr;
+	}
+
+	public static String getbStr() {
+		return bStr;
+	}
+
+	public static String getOp() {
+		return op;
+	}
+
+	public static void setOp(String op) {
+		Engine.op = op;
+	}
+
+}
---
+++ app/src/main/java/rl/ht/be/calc40/utils/Utils.java
@@ -0,0 +1,26 @@
+package rl.ht.be.calc40.utils;
+
+import java.text.NumberFormat;
+
+public class Utils {
+
+	public static Double parseString(String str) {
+		String doubleStr = str.replace(",", ".");
+		return Double.parseDouble(doubleStr);
+	}
+
+	public static String getString(Double d) {
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setGroupingUsed(false);
+		return nf.format(d);
+	}
+
+	public static void main(String[] args) {
+		Double[] a = new Double[] { 1., 1.1, 10.5, 10.0, 1000., 1000.5 };
+		for (Double d : a) {
+			String str = getString(d);
+			System.out.println(d + " --> " + str + " >>> " + parseString(str));
+			System.out.println("=====");
+		}
+	}
+}
